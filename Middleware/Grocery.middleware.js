f2d2f1427d2fdd27a79b3f3ad2b74b9cfb0e9257
AwaisYounas72 <151017136+AwaisYounas72@users.noreply.github.com>
2024-01-01 14:50:33 +0500

    Add files via upload

--- Middleware/Grocery.middleware.js
+++ Middleware/Grocery.middleware.js
@@ -5,8 +5,8 @@ const CreateProduct=[
     check('description').notEmpty().withMessage('The Description is required'),
     check('price').notEmpty().isNumeric().withMessage('The Price  is Required'),
     check('category').notEmpty().withMessage('The category is required'),
-    check('image').notEmpty().withMessage('The image is Required'),
-    check('feature').notEmpty().isArray().withMessage('The features are required'),
+    // check('image').notEmpty().withMessage('The image is Required'),
+    // check('feature').notEmpty().isArray().withMessage('The features are required'),
 
 ]
 
@@ -15,8 +15,8 @@ const UpdateProduct=[
     check('description').optional().notEmpty().withMessage('The Description is Required'),
     check('price').optional().notEmpty().isNumeric().withMessage('The Price  is Required'),
     check('category').optional().notEmpty().withMessage('The category is required'),
-    check('image').optional().notEmpty().withMessage('The image is Required'),
-    check('feature').optional().notEmpty().isArray().withMessage('The features are required'),
+    // check('image').optional().notEmpty().withMessage('The image is Required'),
+    // check('feature').optional().notEmpty().isArray().withMessage('The features are required'),
 ]
 
 export
